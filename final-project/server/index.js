bdfa3900f94a82c2ff793e3db864ee78ab9fefb4
jasminecao <jasminecao03@gmail.com>
2021-04-03 11:19:54 -0400

    change to client server structure

---
+++ final-project/server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+var routes = require("./routes.js");
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+/* ---------------------------------------------------------------- */
+/* ------------------- Route handler registration ----------------- */
+/* ---------------------------------------------------------------- */
+
+app.get('/api/test', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+
+app.listen(8081, () => {
+	console.log(`Server listening on PORT 8081`);
+});
