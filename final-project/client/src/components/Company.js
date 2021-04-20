3ace0bac164b7cec5ec1dadf4ce2a9c80646262e
jasminecao <jasminecao03@gmail.com>
2021-04-19 21:28:31 -0400

    fetch company prices

--- final-project/client/src/components/Company.js
+++ final-project/client/src/components/Company.js
@@ -1,16 +1,42 @@
-import React from 'react';
-import { useLocation } from 'react-router';
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
+import axios from 'axios'
 import NavBar from './NavBar'
 
+// parses URL queries for company name.
+const parseURL = (url) => {
+  if (url.split('/').length > 2) {
+    let company = url.slice(9)
+    // standardize casing
+    company = company.toUpperCase()
+    return company
+  }
+  return ['TEST']
+}
+
 const Company = (props) => {
-  const location = useLocation();
+  // useLocation().pathname will return '/company/ticker'
+  let url = useLocation().pathname
+  const [company, setCompany] = useState(parseURL(url))
+  const [prices, setPrices] = useState([''])
+
+  useEffect(() => {
+    axios.get('http://localhost:8081/company/' + company).then((response) => {
+      setPrices(response.data)
+    })
+  }, [company])
 
   return (
     <div>
       <NavBar />
-      Stonks
+      <h2>Stock Prices for {company}</h2>
+      {prices.map((price) => (
+        <>
+        <p>{price.High}</p>
+        </>
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default Company;
+export default Company
